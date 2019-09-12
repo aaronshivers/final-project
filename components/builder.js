7197bec5d55ff4c5bffb0180ba2e68c5700ec29f
aaron shivers <aaorn@moonmail.org>
2019-09-12 17:46:01 -0500

    setup body property

--- components/builder.js
+++ components/builder.js
@@ -1,6 +1,7 @@
 import {useState, useEffect, useContext} from 'react'
 import AppContext from '../context/context'
 import {startAddCar} from '../actions/cars'
+// import Radio from './radio'
 
 const Builder = () => {
   const {state, dispatch} = useContext(AppContext)
@@ -16,10 +17,13 @@ const Builder = () => {
     startAddCar(car)(dispatch, state)
   }
 
+  useEffect(() => console.log(category))
+
   // useEffect(() => {
   //   setCategory('')
   //   setBody('')
   // }, [state])
+  const handleOnChange = value => setCategory(value)
 
   return (
     <form onSubmit={handleSubmit}>
@@ -44,6 +48,76 @@ const Builder = () => {
         />
         Off Road
       </label>
+      {
+        category === 'street' &&
+        <div>
+          <label>
+            <input
+              type='radio'
+              name='body'
+              value='sport'
+              onChange={e => setBody('sport')}
+              checked={body === 'sport'}
+            />
+            Sport
+          </label>
+          <label>
+            <input
+              type='radio'
+              name='body'
+              value='suv'
+              onChange={e => setBody('suv')}
+              checked={body === 'suv'}
+            />
+            SUV
+          </label>
+          <label>
+            <input
+              type='radio'
+              name='body'
+              value='classic'
+              onChange={e => setBody('classic')}
+              checked={body === 'classic'}
+            />
+            Classic
+          </label>
+        </div>
+      }
+      {
+        category === 'offRoad' &&
+        <div>
+          <label>
+            <input
+              type='radio'
+              name='body'
+              value='atv'
+              onChange={e => setBody('atv')}
+              checked={body === 'atv'}
+            />
+            ATV
+          </label>
+          <label>
+            <input
+              type='radio'
+              name='body'
+              value='duneBuggy'
+              onChange={e => setBody('duneBuggy')}
+              checked={body === 'duneBuggy'}
+            />
+            Dune Buggy
+          </label>
+          <label>
+            <input
+              type='radio'
+              name='body'
+              value='crawlers'
+              onChange={e => setBody('crawlers')}
+              checked={body === 'crawlers'}
+            />
+            Crawlers
+          </label>
+        </div>
+      }
       <button type='submit'>Add to Cart</button>
     </form>
   )
