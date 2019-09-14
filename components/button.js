c8edb42129cb7b103e11e88b384f08e9b2d1875f
aaronshivers <aaron@moonmail.org>
2019-09-14 14:41:15 -0500

    utilized button component on cart page

--- components/button.js
+++ components/button.js
@@ -1,22 +1,24 @@
-const Button = ({text}) => {
-    return (
-        <button>{text}
-        <style jsx>{`
-            button {
-                background-color: white;
-                font-family: arial;
-                font-size: 1.2rem;
-                padding: .6rem;
-                border: 2px solid black;
-                border-radius: 5px;
-            }
-            button:hover {
-                background-color: gray;
-                cursor: pointer;
-            }
-        `}</style>
-        </button>
-    );
+const Button = ({text, id, handleButtonClick}) => {
+  return (
+    <button
+      onClick={() => handleButtonClick(id)}
+    >{text}
+    <style jsx>{`
+        button {
+            background-color: white;
+            font-family: arial;
+            font-size: 1.2rem;
+            padding: .6rem;
+            border: 2px solid black;
+            border-radius: 5px;
+        }
+        button:hover {
+            background-color: gray;
+            cursor: pointer;
+        }
+    `}</style>
+    </button>
+  );
 }
 
 export default Button;
--- pages/cart.js
+++ pages/cart.js
@@ -3,10 +3,13 @@ import AppContext from '../context/context'
 import Nav from '../components/nav'
 import CartItem from '../components/cart-item'
 import {startRemoveCar} from '../actions/cars'
+import Button from '../components/button'
 
 
 const Cart = () => {
   const {state, dispatch} = useContext(AppContext)
+
+  const handleButtonClick = id => startRemoveCar(id)(dispatch, state)
 
   return (
     <>
@@ -17,7 +20,7 @@ const Cart = () => {
           state[0] && state.map((car) => (
             <div key={car.id}>
               <CartItem {...car} />
-              <button onClick={() => startRemoveCar(car.id)(dispatch, state)}>X</button>
+              <Button text='X' id={car.id} handleButtonClick={handleButtonClick} />
             </div>
           ))
         }
